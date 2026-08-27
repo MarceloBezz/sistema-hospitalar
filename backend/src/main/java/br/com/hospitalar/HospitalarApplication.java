3df151b3ff04ce7858d7c77bca8abd3d7feed453
Marcelo Bezerra <mdbezerra16@gmail.com>
2026-08-27 11:02:29 -0300

    :tada: Início do projeto

---
+++ backend/src/main/java/br/com/hospitalar/HospitalarApplication.java
@@ -0,0 +1,13 @@
+package br.com.hospitalar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalarApplication.class, args);
+	}
+
+}
